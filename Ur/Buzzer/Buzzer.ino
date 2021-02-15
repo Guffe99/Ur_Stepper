495d5148ac4e6fcb5d78d099f696cb5bae983b28
Guffe99 <54105735+Guffe99@users.noreply.github.com>
2021-02-15 19:26:15 +0100

    ServoRobotSample
    Plz work

--- Ur/Buzzer/Buzzer.ino
+++ Ur/Buzzer/Buzzer.ino
@@ -1,13 +1,13 @@
-#define BUZZER 4
+#define buzz 4
 
 void setup() {
   // put your setup code here, to run once:
-pinMode(BUZZER, OUTPUT);
+pinMode(buzz, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-if (sek == 0 && min == 0){
+if (sek == 0 && min == 0 && buzz ==){
   digitalWrite(BUZZER,HIGH);
   delay(1000);
   digitalWrite(BUZZER,LOW);}
