a5cafe75c178424140182eb6c46e34249472e4c3
Nichlas Overgaard Laugesen <nl@hldata.dk>
2021-02-19 19:56:33 +0100

    final.final.final.FORREAL

--- Ur/Ur.ino
+++ Ur/Ur.ino
@@ -86,6 +86,37 @@ void refreshLCD()
   lcd.setCursor(0,0);
   lcd.print("Tid:");
 
+  if (T < 3){                              // Conditional statements checking current Time State
+    if (timeMin >= 100){
+      lcd.setCursor (5,0);
+    }
+
+    if (timeMin < 100 && timeMin >= 10){
+      lcd.setCursor (5,0);
+      lcd.print(" ");
+      lcd.setCursor (6,0);
+    }
+
+    if (timeMin < 10){
+      lcd.setCursor (6,0);
+      lcd.print(" ");
+      lcd.setCursor (7,0);
+    }
+
+    lcd.print(timeMin);
+    lcd.setCursor (8,0);
+    lcd.print(":");
+
+    if ((timeSek - 60 * timeMin) >= 10){
+      lcd.setCursor (9,0);
+    }
+
+    if ((timeSek - 60 * timeMin) < 10){
+      lcd.setCursor (9,0);
+      lcd.print(0);
+      lcd.setCursor (10,0);
+    }
+    lcd.print(timeSek - 60 * timeMin);
   if (timeMin >= 100){
     lcd.setCursor (5,0);
   }
